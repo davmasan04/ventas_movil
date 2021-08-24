908b253210a4feb31d75bb8e4f45a5e252a57a5e
Katiuska <kmmarin@espol.edu.ec>
2021-08-24 07:03:05 -0500

    vistas de vendedor con bootstrap

--- BackMovil/usuarios/urls.py
+++ BackMovil/usuarios/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('vendedor/', views.vendedor, name='vendedor'),
+    path('login/', views.index, name='login')
 ]
--- BackMovil/usuarios/views.py
+++ BackMovil/usuarios/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
+from django.template import loader
 from .models import Vendedor, Usuario
+from django import template
 
 # Create your views here.
 def vendedor(request):
@@ -8,3 +11,5 @@ def vendedor(request):
   context = {'vendedores':vendedors}
   return render(request, 'usuarios/vendedor.html', context) 
 
+def index(request):
+  return render(request, 'cuentas/login.html')
